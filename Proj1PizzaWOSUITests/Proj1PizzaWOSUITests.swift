97b66fa2e2e440b0c44711a798dafbb6d90f696b
Francisco Ulises Roldan Trejo <UlisesRoldan@macbook-pro-de-francisco.local>
2016-09-03 13:16:51 -0500

    Initial Commit

---
+++ Proj1PizzaWOSUITests/Proj1PizzaWOSUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Proj1PizzaWOSUITests.swift
+//  Proj1PizzaWOSUITests
+//
+//  Created by Francisco Ulises Roldan Trejo on 03/09/16.
+//  Copyright © 2016 Francisco Ulises Roldan Trejo. All rights reserved.
+//
+
+import XCTest
+
+class Proj1PizzaWOSUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
