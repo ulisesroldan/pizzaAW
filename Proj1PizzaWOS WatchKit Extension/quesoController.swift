0daef616af544070ab5c513df35db4f4d65d3db3
Francisco Ulises Roldan Trejo <UlisesRoldan@macbook-pro-de-francisco.local>
2016-09-07 17:33:43 -0500

    Actualiza

--- Proj1PizzaWOS WatchKit Extension/quesoController.swift
+++ Proj1PizzaWOS WatchKit Extension/quesoController.swift
@@ -11,13 +11,57 @@ import Foundation
 
 
 class quesoController: WKInterfaceController {
+    var pickerDataSource = ["Mozarela", "Cheddar", "Parmesano", "Sin queso"];
+    var pickerItems:[WKPickerItem]?
+    var quesoSel = "Mozarela"
+    var pedido:[String]?
+    @IBOutlet var quesoPicker: WKInterfacePicker!
 
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
+        pedido = context as? [String]
+        if let pedidoPrev = pedido {
+            print("El pedido")
+            for index in pedidoPrev {
+                print(index)
+            }
+        }
+
+        pickerItems = pickerDataSource.map(){
+            let pickerItem = WKPickerItem()
+            pickerItem.title = $0
+            pickerItem.caption = $0
+            return pickerItem
+        }
+        quesoPicker.setItems(pickerItems)
         // Configure interface objects here.
     }
 
+    @IBAction func pasaIngred() {
+        if (!pedido!.isEmpty) {
+            pedido = pedido!.filter(){
+                $0 != quesoSel
+            }
+        }
+        pedido!.append(quesoSel)
+        self.pushControllerWithName("vistaIngred", context: pedido)
+    }
+    @IBAction func eligeQueso(value: Int) {
+        if (!pedido!.isEmpty) {
+            pedido = pedido!.filter(){
+                $0 != quesoSel
+            }
+        }
+        //Eliminar esta lìnea
+        NSLog("List Picker: \(pickerDataSource[value]) selected")
+        NSLog("pedido: \(pedido)  selected")
+        let pickedItem = pickerItems![value]
+        if pickedItem.title != nil {
+            quesoSel = pickedItem.title!
+        }
+
+    }
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
